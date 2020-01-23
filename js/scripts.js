18796db63c1ab01bd19652404eec513692369c47
Jack Dunning, Krista Rutz and Steven Fleming <jack+krista+steven@>
2020-01-23 11:10:26 -0800

    if first letter is a vowel returns way at the end of string is first letter is not eliminates first word and first charecter to the end allong with ay. Also applies the conditions for a vowel to single letter words

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,20 @@
 
 function translateToPL(str){
 
-  var firstcharstr = str.charAt(0);
-  var restofstr = str.substr(1);
-  var pigstr = restofstr + firstcharstr + "ay";
+  var restOfStr = str.substr(1);
+  var firstCharStr = str.charAt(0);
+
+  if (str.length === 1) {
+    var pigStr = str + "ay";
+  } else if (firstCharStr === "a" || firstCharStr ===  "e" || firstCharStr === "i" || firstCharStr === "o" ||firstCharStr ===  "u") {
+    var pigStr = str + "way";
+  } else {
+    var pigStr = restOfStr + firstCharStr + "ay";
+  }
   
-  console.log(pigstr)
-  return pigstr;
+  console.log(pigStr)
+  return pigStr;
 }
-
-// function translateToPL("i"){
-//   return "i";
-//   "st" += ("ay")
-// }
-
 
 // User Interface Logic
 
